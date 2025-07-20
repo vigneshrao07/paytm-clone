cad3aeca966ccfbea33a0db7bac636193c979296
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-07-20 17:06:04 +0100

    listenport

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
- const mainRouter=require("./routes/index");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const mainRouter = require("./routes/index");
 
- const app=express();
+const app = express();
 
- app.use("/api/v1", mainRouter);
+app.use("/api/v1", mainRouter);
 
+app.listen(3000);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,3 +5,5 @@ const router = express.Router();
 
 router.use("/user", userRouter);
 module.exports = router;
+
+
