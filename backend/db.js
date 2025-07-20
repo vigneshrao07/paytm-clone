449718a247caa7614654198a9e37c2d01fc35a52
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-07-20 16:15:39 +0100

    commit

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://vignesh45:Rohitsharma%401@cluster0.levgxkf.mongodb.net/paytmdatabase"
+);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxlength: 50,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = {
+  User,
+};
