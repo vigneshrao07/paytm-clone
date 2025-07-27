994f8f5827ba239948f75234b9bfa7e9b2381e3e
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-07-27 22:23:03 +0100

    commit

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@ mongoose.connect(
   "mongodb+srv://vignesh45:Rohitsharma%401@cluster0.levgxkf.mongodb.net/paytmdatabase"
 );
 
+
+//schema for user table
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const userRouter = require("./user");
 
-const router = express.Router();
+const router = express.Router();    //this router this willhandle all the requst 
 
 router.use("/user", userRouter);
 module.exports = router;
 
-
+//why we created router? because we know all requiest going to 
+//all request will start from  /api/v1/user /api/v1/transactions....
+//in index.js where we will create our express server all request to api/v1/please go to this router 
+//app.use is used to fun middlewares before functions also  used to route request that start with substring over to another router
